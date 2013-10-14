4bebdb17ab8276b4f1e04e762e87d42e6698a710
Yuta Yamada <cokesboy@gmail.com>
2013-10-14 14:20:46 -0400

    Add comments

--- helm-ag-r.el
+++ helm-ag-r.el
@@ -31,17 +31,26 @@
 ;; (setq helm-ag-r-option-list
 ;;       '("-S -U --hidden"
 ;;         "-S -U -l"))
+;; To use helm-ag-r-google-contacts-list command, specify your google
+;; mail address to helm-ag-r-google-contacts-user variable.(If you
+;; specified gmail-address to user-mail-address, then you don't need
+;; below configuration.)
+;; (setq helm-ag-r-google-contacts-user "")
+;; And if you are Japanese, to use Japanese language set below configuration.
+;; this variable set $LANG environment variable by default.
+;; (setq helm-ag-r-google-contacts-lang "ja_JP.UTF-8")
 
 ;; Commands
 ;; helm-ag-r-current-file -- search from current file
 ;; helm-ag-r-from-git-repo -- search from git repository
 ;; helm-ag-r-shell-history -- search shell history
 ;; helm-ag-r-git-logs -- search git logs
+;; helm-ag-r-google-contacts-list -- show your google-contacts
 
 (eval-when-compile (require 'cl))
 (require 'helm)
 
-;; User customize variable
+;; User customize variables
 (defvar helm-ag-r-option-list '()
   "This variable is utilize as ag's option.
  Example:
@@ -51,7 +60,8 @@
 
 (defvar helm-ag-r-histfile
   (shell-command-to-string "echo -n $HISTFILE")
-  "history file to use at helm-ag-r-shell-history function")
+  "History file to use at helm-ag-r-shell-history function.
+ default is specified $HISTFILE.")
 
 (defvar helm-ag-r-google-contacts-user
   (let ((case-fold-search nil))
